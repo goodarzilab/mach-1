c3a97b4fb22c145bff5cc58f834bb2e5f0aa33f2
Ali Saberi <saberi@hey.com>
2025-11-25 11:24:30 -0800

    updated with latest dependencis

--- training-model/mach_dependencies.sh
+++ training-model/mach_dependencies.sh
@@ -2,18 +2,24 @@ conda create -p /envs/mach-1 -y
 conda activate /envs/mach-1
 
 # pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
-conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
+pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cu121
+# pip install flash-attn==2.5.8 --no-build-isolation
 pip install packaging
 pip install ninja
 
 mkdir /envs/mach-1/sources
 
 git clone https://github.com/Dao-AILab/flash-attention.git
-cd flash-attention 
-python setup.py install
-cd csrc/rotary 
+cd flash-attention
+git checkout v2.5.8
+
+# Set CUDA architectures for your H100
+export TORCH_CUDA_ARCH_LIST="8.0;9.0"
+export MAX_JOBS=4
+
+cd csrc/layer_norm
 pip install .
-cd ../layer_norm 
+cd ../csrc/rotary
 pip install .
 
 cd /envs/mach-1/sources
@@ -29,3 +35,5 @@ cd /envs/mach-1
 pip install triton
 pip install evo-model
 pip install transformers tokenizers accelerate datasets evaluate
+pip install wandb
+pip install scipy
